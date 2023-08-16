2f0489a9d5331820ba7cfc7d77923dad71055b12
Nikolay Sholokhov <devilzick@gmail.com>
2023-08-16 16:22:31 +0300

    first commit

---
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -0,0 +1,30 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class LoginPage {
+
+    private final SelenideElement loginField = $("[data-test-id=login] input");
+    private final SelenideElement passwordField = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage(); // Вернуть экземпляр VerificationPage
+    }
+
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage(); // Вернуть экземпляр DashboardPage
+    }
+}
